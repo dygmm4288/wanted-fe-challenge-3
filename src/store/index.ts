1e054ee46b6a4f942f715e3fea73eec0c9456960
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-02-27 16:03:50 +0900

    [add] : todoSlice store에 등록

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./todoSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { todo: todoReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;

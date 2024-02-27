4d7ffb392c9e4c970acc668854b2f1db6feab0de
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-02-27 21:27:15 +0900

    [add] : global css설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
+import GlobalStyles from "./GlobalStyles";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <GlobalStyles />
     <Provider store={store}>
       <App />
     </Provider>

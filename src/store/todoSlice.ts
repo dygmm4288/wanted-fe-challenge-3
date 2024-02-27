bde3ec84ca119435b80d10ada1dbf466e8644a21
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2024-02-27 16:03:05 +0900

    [add] : todoSlice 추가

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,37 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type Todo = {
+  id: number;
+  content: string;
+  isDone: boolean;
+};
+
+const initialState = {
+  todos: [] as Todo[],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      const todo = action.payload;
+      state.todos.push(todo);
+    },
+    deleteTodo: (state, action: PayloadAction<number>) => {
+      const id = action.payload;
+      const index = state.todos.findIndex((todo) => todo.id === id);
+
+      state.todos.splice(index, 1);
+    },
+    toggleTodo: (state, action: PayloadAction<number>) => {
+      const id = action.payload;
+      const index = state.todos.findIndex((todo) => todo.id === id);
+
+      state.todos[index].isDone = !state.todos[index].isDone;
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, toggleTodo } = todoSlice.actions;
+export default todoSlice.reducer;
